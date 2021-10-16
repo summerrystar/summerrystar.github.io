a051e358b5291f9712ed7537c0f369653ec1dec1
Chengjia Shao <chengjia.shao@skyworksinc.com>
2021-10-16 18:24:34 -0500

    Added Chinese attendance calendar

---
+++ Chinese/calendar_script.js
@@ -0,0 +1,91 @@
+const date = new Date();
+
+var attendance = [
+  new Date(2021, 9, 4),
+  new Date(2021, 9, 13),
+  new Date(2021, 9, 16)
+];
+
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "一月",
+    "二月",
+    "三月",
+    "四月",
+    "五月",
+    "六月",
+    "七月",
+    "八月",
+    "九月",
+    "十月",
+    "十一月",
+    "十二月",
+  ];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()] + " " + date.getFullYear();
+
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    let mark = 0;
+    for (const att of attendance){
+      if (i === att.getDate() && date.getMonth() === att.getMonth()){
+        
+        days += `<div class="today">${i}</div>`;
+        mark = 1;
+        break;
+      }
+    }
+    if (mark === 0) {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+renderCalendar();
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
