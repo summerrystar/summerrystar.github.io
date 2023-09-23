f75617ac64e4c19bf52227056dd56f616458046a
Chengjia Shao <cjshao@umich.edu>
2023-09-23 18:34:01 -0500

    Chinese update

--- Chinese/calendar_script.js
+++ Chinese/calendar_script.js
@@ -52,7 +52,8 @@ var attendance = [
   new Date(2023, 1, 9),
   new Date(2023, 1, 13),
   new Date(2023, 2, 5),
-  new Date(2023, 2, 26)
+  new Date(2023, 2, 26),
+  new Date(2023, 8, 23)
 ];
 
 const renderCalendar = () => {
